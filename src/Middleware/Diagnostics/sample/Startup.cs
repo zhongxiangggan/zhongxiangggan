d4e022d81dcaa175d04c37f5034be7381c2ecea5
John Luo <johluo@microsoft.com>
2021-07-22 14:22:20 -0700

    Wip

---
+++ src/Middleware/Diagnostics/sample/Startup.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseExceptionHandler("/error");
+
+app.MapGet("/error", () => "oh no!");
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/throw", IResult () =>
+{
+    throw new Exception("bad bad");
+});
+
+app.Run();
